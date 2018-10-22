005fd99f9b08e0b23f9f2019cd5f174d1f10fcf9
badojo <pdj10211@gmail.com>
2018-10-23 02:44:43 +0900

    블록체인 cmd패키지(cli,명령툴) 구현

---
+++ src/gc_cmd/cli.go
@@ -0,0 +1,77 @@
+/**
+gc_cmd 패키지 :
+
+cli 파일을 담고 있다.
+커맨드라인에서 명령어를 통해 블록체인 코어를 실행하고 조절하는 go 파일을 담고있다.
+ethereum으로 비교하자면 console 패키지와 같다.
+
+ */
+package gc_cmd
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+
+	"../src/gccore"
+)
+
+type CLI struct {
+	bc *Blockchain
+}
+
+
+func (cli *CLI) Run() {
+	cli.validateArgs()
+
+	addBlockCmd := flag.NewFlagSet("addblock", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
+	addBlockData := addBlockCmd.String("data", "", "Block data")
+
+	switch os.Args[1] {
+	case "addblock":
+		err := addBlockCmd.Parse(os.Args[2:])
+	case "printchain":
+		err := printChainCmd.Parse(os.Args[2:])
+	default:
+		cli.printUsage()
+		os.Exit(1)
+	}
+
+	if addBlockCmd.Parsed() {
+		if *addBlockData == "" {
+			addBlockCmd.Usage()
+			os.Exit(1)
+		}
+		cli.addBlock(*addBlockData)
+	}
+
+	if printChainCmd.Parsed() {
+		cli.printChain()
+	}
+}
+
+func (cli *CLI) addBlock(data string) {
+	cli.bc.AddBlock(data)
+	fmt.Println("Success!")
+}
+
+func (cli *CLI) printChain() {
+	bci := cli.bc.Iterator()
+
+	for {
+		block := bci.Next()
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+
+		if len(block.PrevBlockHash) == 0 {
+			break
+		}
+	}
+}
