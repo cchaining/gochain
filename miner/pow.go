9138227b5a8c89bcaaab2ebb6a291f30f5c30de1
badojo <pdj10211@gmail.com>
2018-10-27 03:26:46 +0900

    패키지명 rename, go파일 패키지 의존성 변경

---
+++ miner/pow.go
@@ -0,0 +1,94 @@
+package miner
+
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"math"
+	"math/big"
+
+	"../core"
+)
+
+
+var (
+	maxNonce = math.MaxInt64
+)
+
+const targetBits = 24
+
+type ProofOfWork struct {
+	block *core.Block
+	target *big.Int
+}
+
+
+func NewProofOfWork(b *core.Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+
+	pow := &ProofOfWork{b, target}
+	return pow
+}
+
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.Data,
+			IntToHex(pow.block.Timestamp),
+			IntToHex(int64(targetBits)),
+			IntToHex(int64(nonce)),
+		},
+		[]byte{},
+	)
+	return data
+}
+
+
+/*
+
+	1. 데이터 준비 (생성)
+	2. SHA-256 해싱
+	3. 해시값의 큰 정수로의 변환
+	4. 정수값과 타겟값 비교
+
+ */
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+
+	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
+
+	for nonce < maxNonce {
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+
+		hashInt.SetBytes(hash[:])
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Print("\n\n")
+
+	return nonce, hash[:]
+}
+
+
+func (pow *ProofOfWork) Validate() bool {
+	var hashInt big.Int
+
+	data := pow.prepareData(pow.block.Nonce)
+	hash := sha256.Sum256(data)
+	hashInt.SetBytes(hash[:])
+
+	isValid := hashInt.Cmp(pow.target) == -1
+	return isValid
+}
+
